c275e22bde79bd56b99595045ebe3d04e31dd1ea
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-29 02:58:01 -0400

    Update index.js
    Adding another path to see if it works.

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.get('/', function (req, res) {
 	res.send("Hello World!, I am server created by expresss"); 
 }) 
 
+// Testing custom path
+app.get('/bro/', function (req, res) { 
+	res.send("Ayo!, I'm a new path in this ExpressJS app. What's good?"); 
+}) 
+
 // Listening to server at port 3000 
 app.listen(PORT, function () { 
 	console.log("server started"); 
