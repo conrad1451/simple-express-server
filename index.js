88f5e07aa8f18a36189f0929c807bc21753e75c3
Conrad Hansen-Quartey <Chansenq@gmail.com>
2024-06-29 02:48:49 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const express = require('express');
 // Creating instance of express 
 const app = express(); 
 
+const PORT = process.env.PORT || 8081;
+
 // Handling GET / Request 
 app.get('/', function (req, res) { 
 	res.send("Hello World!, I am server created by expresss"); 
 }) 
 
 // Listening to server at port 3000 
-app.listen(3000, function () { 
+app.listen(PORT, function () { 
 	console.log("server started"); 
 })
